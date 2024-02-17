4652691c1a761fcdec96e01fdb79a843ff3b7aa4
littlesekii <littlesekii@gmail.com>
2024-02-17 16:24:30 -0300

    project started

---
+++ src/test/java/com/littlesekii/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.littlesekii.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
